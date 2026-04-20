de8e1d69b2dd754e3accae449b9f7f74fc87efe9
EESTN1 <benjaminviskovich2@gmail.com>
2026-04-20 19:28:15 -0300

    talle lista 2

--- lista.js
+++ lista.js
@@ -28,7 +28,7 @@ function crearFilaCorredor(id, datos) {
     const li = document.createElement("li");
     
     const texto = document.createElement("span");
-    texto.textContent = `${datos.nombre} ${datos.apellido} | DNI: ${datos.DNI} | [Talle: ${datos.talle}]`;
+    texto.textContent = `${datos.nombre} ${datos.apellido} | DNI: ${datos.DNI} | [Talle: ${datos.talle || 'S/N'}]`;
 
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
